87766795dda8f78bf4d15673bc8dee262cd9c2ab
Patrick James Dionen <130761529+ImTrikk@users.noreply.github.com>
2023-10-25 23:29:06 +0800

    Update socket.js

--- client/socket/socket.js
+++ client/socket/socket.js
@@ -1,7 +1,7 @@
 // socket.js
 import io from "socket.io-client";
 
-// const socket = io.connect("https://simpli-chat-server.vercel.app/");
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://simpli-chat-server.vercel.app");
+//const socket = io.connect("http://localhost:3001");
 
 export default socket;
